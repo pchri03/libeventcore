31a0222713b9564882f2ac2abc21f800595cc900
Peter Christensen <pch@ordbogen.com>
2014-06-24 11:41:33 +0200

    Relicense to 2-clause BSD license

--- include/eventcore/timer.h
+++ include/eventcore/timer.h
@@ -1,19 +1,7 @@
 /*
- * Copyright (C) 2014 Peter Christensen <pch@ordbogen.com>
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, write to the Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ * Copyright (c) 2014 Ordbogen A/S
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
  */
 
 #ifndef INCLUDE_EVENTCORE_TIMER_H
